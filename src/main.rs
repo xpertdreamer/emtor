90b2d51670ae8c73e63e4649a627db45c4996ed3
xpertdreamer <yungnomen@gmail.com>
2026-07-27 17:10:44 +0300

    HALT test

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let mut emtor = Cpu::create();
     emtor.regs.a = 5;
     emtor.regs.b = 6;
-    emtor.load_prog(&[1, 1, 1, 0]);
+    emtor.load_prog(&[1, 1, 1, 0, 1]);
     emtor.run();
     println!("reg a: {}", emtor.regs.a);
     println!("reg b: {}", emtor.regs.b);
