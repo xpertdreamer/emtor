8e4559af20f5925ae4133b30c2db3838bbd844f9
xpertdreamer <yungnomen@gmail.com>
2026-07-29 23:01:36 +0300

    One simple test for MUL

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,11 @@ fn main() {
     emtor.load_prog(&[
         0x02, 0x0B, 0x00, 12,    // MOV Const
         0x02, 0x0B, 0x01, 2,     // MOV Const
-        0x01,                    // ADD: A = A + B
-        0x02, 0x0A, 0x01, 0x00,  // MOV Reg (0x0A)
+        0x01,                    // ADD
+        0x02, 0x0A, 0x01, 0x00,  // MOV Reg
         0x02, 0x0B, 0x01, 0x04,  // MOV Const
         0x03,                    // SUB
+        0x04,                    // MUL
         0x00                     // HLT
     ]);
 
@@ -41,11 +42,25 @@ mod tests {
         emtor.load_prog(&[
             0x02, 0x0B, 0x00, 12,    // MOV Const
             0x02, 0x0B, 0x01, 2,     // MOV Const
-            0x01                     // ADD: A = A + B
+            0x01,                    // ADD
         ]);
         emtor.run();
         assert_eq!(emtor.regs.a, 14);
         assert_eq!(emtor.regs.b, 2);
         assert_eq!(emtor.pc, 10);
+    }
+
+    #[test]
+    fn test_mul() {
+        let mut emtor = Cpu::create();
+        emtor.load_prog(&[
+            0x02, 0x0B, 0x00, 0x32,     // MOV Const
+            0x02, 0x0B, 0x01, 0x02,     // MOV Const
+            0x04                        // MUL
+        ]);
+        emtor.run();
+        assert_eq!(emtor.regs.a, 0x64);
+        assert_eq!(emtor.regs.b, 0x02);
+        assert_eq!(emtor.pc, 0x0A);
     }
 }
