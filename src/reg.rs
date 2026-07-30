60823123e920ed4e8aafff25ba0830635f325d09
xpertdreamer <yungnomen@gmail.com>
2026-07-30 15:43:30 +0300

    New flags-mask-register and function to zero it

--- src/reg.rs
+++ src/reg.rs
@@ -1,13 +1,20 @@
 pub const REG_A: u8 = 0x00;
 pub const REG_B: u8 = 0x01;
 
+
+
 pub struct Regs {
     pub a: u8,
-    pub b: u8
+    pub b: u8,
+    pub f: u8
 }
 
 impl Regs {
     pub fn create() -> Self {
-        Regs { a: 0, b: 0 }
+        Regs { a: 0, b: 0, f: 0}
+    }
+
+    pub fn f_zeroed(&mut self) {
+        self.f = 0;
     }
 }
