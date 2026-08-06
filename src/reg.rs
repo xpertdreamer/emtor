1436d19269a18d9ebf9d2b4b5f60fed14f666501
xpertdreamer <yungnomen@gmail.com>
2026-08-06 23:11:25 +0300

    Mark register 'r' as unused for some time

--- src/reg.rs
+++ src/reg.rs
@@ -6,15 +6,15 @@ pub struct Regs {
     pub a: u8,
     pub b: u8,
     pub c: u8,
-    #[allow(unused)]
-    pub r: u8,
+    // NOTE: I haven't decided yet whether it's needed
+    pub _r: u8,
     pub f: u8,
     pub sf: u8
 }
 
 impl Regs {
     pub fn create() -> Self {
-        Regs { a: 0, b: 0, c: 0, r: 0, f: 0, sf: 0 }
+        Regs { a: 0, b: 0, c: 0, _r: 0, f: 0, sf: 0 }
     }
 
     pub fn f_zeroed(&mut self) {
