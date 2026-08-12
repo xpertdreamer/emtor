33a2e1c0d446f01a9c6ac3e80b9604f58b3e450a
xpertdreamer <yungnomen@gmail.com>
2026-08-12 21:56:38 +0300

    Thanks magit

--- src/machine.rs
+++ src/machine.rs
@@ -128,22 +128,22 @@ impl Machine {
 
     fn exec_sht(&mut self, data: u8, dest: u8) {
         // TODO: trace
-        let dir: u8 = (data & NEGATIVE_U8) >> 7;
-        let x: u8 = data & !NEGATIVE_U8;
-        let mut value = self.cpu.read_reg(dest).expect("ERROR: [SHT] invalid src register ID");
+        let neg = data & NEGATIVE_U8;
+        let dir: u8 = neg >> 7;
+        let x: u8 = (data & !NEGATIVE_U8) % 8;
+        let mut value: i8 = self.cpu.read_reg(dest).expect("ERROR: [SHT] invalid src register ID");
+        let mut val_u: u8 = value as u8;
         match dir {
-            0 => value = value << x,
-            1 => value = value >> x,
-            _ => {
-                eprintln!("ERROR: [SHT] error occured while decoding direction");
-                self.cpu.halt();
-                return;
-            }
+            0 => val_u = val_u << x,
+            1 => val_u = val_u >> x,
+            _ => unreachable!()
         };
+        value = ((val_u & !NEGATIVE_U8) | neg) as i8;
         if !self.cpu.write_reg(dest, value) {
             eprintln!("ERROR: cannot write SHT result to {}",  match dest { REG_A => "A", REG_B => "B", REG_C => "C", _ => "?" });
             self.cpu.halt();
         }
+        self.trace("SHT");
     }
 
     // * * *
